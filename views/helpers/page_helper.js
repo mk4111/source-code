66a020eb4d4a61f97aef6f0ca123644de4789495
speedingdeer <ffwisniewski@gmail.com>
2016-09-20 15:07:49 +0200

    Fixed home page pagination. Tweaked query hendler code.

--- views/helpers/page_helper.js
+++ views/helpers/page_helper.js
@@ -1,19 +1,25 @@
 var Handlebars = require('handlebars');
-module.exports = function (page, prev, next, totalPages, queryString) {
+module.exports = function (page, totalPages, queryString) {
 
   // @speedingdeer: have no idea what is this for
   page = Handlebars.Utils.escapeExpression(page);
-  prev = Handlebars.Utils.escapeExpression(prev);
-  next = Handlebars.Utils.escapeExpression(next);
   totalPages = Handlebars.Utils.escapeExpression(totalPages);
 
   // @TODO: this is lame, html must be render in template, injecting like here feels weird
 
+  if(totalPages == 0) { return ""; }
+
   var result = "<div class='ui pagination menu'>";
+
+  var pagePath = "query/"; 
+  if (!queryString) { 
+    pagePath = "";
+    queryString = ""; 
+  }
 
   if (Number(page) > 1) {
     // append the fisrt page
-    result += "<a class='item' href='/query/" + "1" + queryString + "'>1</a>";
+    result += "<a class='item' href='/" + pagePath + "1" + queryString + "'>1</a>";
   }
 
   if (Number(page) > 3) {
@@ -23,16 +29,16 @@ module.exports = function (page, prev, next, totalPages, queryString) {
 
   if (Number(page) > 2) {
     // append the ... between first and current
-      result += "<a class='item' href='/query/" + ( Number(page) - 1 ) + queryString + "'>" + ( Number(page) - 1 ) + "</a>";
+      result += "<a class='item' href='/" + pagePath + ( Number(page) - 1 ) + queryString + "'>" + ( Number(page) - 1 ) + "</a>";
   }
 
 
   // now add current
-  result += "<a class='active item' href='/query/" + page + queryString + "'>" + page + "</a>";
+  result += "<a class='active item' href='/" + pagePath + page + queryString + "'>" + page + "</a>";
 
   if (Number(page) < Number(totalPages) - 1 ) {
     // append the ... between the current and last
-    result += "<a class='item' href='/query/" + ( Number(page) + 1 ) + queryString + "'>" + ( Number(page) + 1 ) + "</a>";
+    result += "<a class='item' href='/" + pagePath + ( Number(page) + 1 ) + queryString + "'>" + ( Number(page) + 1 ) + "</a>";
   }
 
   if (Number(page) < Number(totalPages) - 2 ) {
@@ -42,7 +48,7 @@ module.exports = function (page, prev, next, totalPages, queryString) {
 
   if (Number(page) < Number(totalPages) ) {
     // append the last
-      result += "<a class='item' href='/query/" + totalPages + queryString + "'>" + totalPages + "</a>";
+      result += "<a class='item' href='/" + pagePath + totalPages + queryString + "'>" + totalPages + "</a>";
   }
 
   result += "</div>";
