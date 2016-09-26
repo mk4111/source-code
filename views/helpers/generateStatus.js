8887f843182bc173cebf67ad51fd376179c71eb3
speedingdeer <ffwisniewski@gmail.com>
2016-09-26 16:20:56 +0200

    Extract the candidates list stages from mongo not ES. We are going to drop ES candidate stateges soon.

--- views/helpers/generateStatus.js
+++ views/helpers/generateStatus.js
@@ -3,15 +3,16 @@ module.exports = function (statusCurrent, clients, stages) {
 
 
   var result = "";
-  
+
+
   if (!statusCurrent || !clients || !stages) { return ""; }
 
   clients.forEach(function (obj) {
-    if (statusCurrent.idClient.toString() === obj.id.toString()) { result += obj.name + " "; }
+    if (statusCurrent.clientId.toString() === obj.id.toString()) { result += obj.name + " "; }
   });
 
-  if (String(statusCurrent.idStage) in stages) {
-    result += "[" + stages[String(statusCurrent.idStage)].name + "]";
+  if (String(statusCurrent.stageId) in stages) {
+    result += "[" + stages[String(statusCurrent.stageId)].name + "]";
   }
 
   return new Handlebars.SafeString(result);
