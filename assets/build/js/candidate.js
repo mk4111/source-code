56dc7041ec58b570927f87251b11458bbe6d4224
speedingdeer <ffwisniewski@gmail.com>
2016-09-27 19:58:35 +0200

    Deployed assets.

--- assets/build/js/candidate.js
+++ assets/build/js/candidate.js
@@ -2,14 +2,25 @@ $(function() {
   if ($(".container.candidate").length === 0) {
     return;
   }
-  return $(".container.candidate .action-buttons button").each(function() {
+  $(".container.candidate .action-buttons button").each(function() {
     var button, modal;
     button = $(this);
     if (button.val()) {
       modal = button.parent().find(".modal." + button.val());
       return button.click(function() {
-        return modal.modal('show');
+        modal.modal('show');
+        return false;
       });
     }
+  });
+  return $(".modal.send-email").each(function() {
+    var initial_message, modal;
+    modal = $(this);
+    initial_message = modal.find("textarea[name='message']").val();
+    return modal.find(".actions button").click(function() {
+      modal.find("form.sendmail").submit();
+      modal.find("form.sendmail input[name='subject']").val("");
+      return modal.find("form.sendmail input[name='message']").val(initial_message);
+    });
   });
 });
