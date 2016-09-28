fcb48cf6dd2e6e20abf1fb9a2d6480a7ed539e3a
speedingdeer <ffwisniewski@gmail.com>
2016-09-28 13:05:20 +0200

    Deployed assets.

--- assets/build/js/dashboard.js
+++ assets/build/js/dashboard.js
@@ -130,7 +130,7 @@ $(function() {
       return filter_results();
     }
   });
-  $('.ui.dropdown.roles').dropdown({
+  return $('.ui.dropdown.roles').dropdown({
     onChange: function(f) {
       if (f.length) {
         filters.roles = f.split(',');
@@ -139,29 +139,5 @@ $(function() {
       }
       return filter_results();
     }
-  });
-  $(".dashboard .list_candidats .candidate-box-wrap .buttons.stage-actions button").each(function() {
-    return $(this).popup({
-      hoverable: true,
-      inline: true
-    });
-  });
-  return $(".dashboard .list_candidats .candidate-box-wrap .buttons.stage-actions > button").each(function() {
-    var button, form, modal;
-    button = $(this);
-    modal = button.parent().find(".modal." + button.val());
-    form = button.parent().find("form." + button.val());
-    modal.find(".button.cancel").click(function() {
-      return modal.modal('close');
-    });
-    modal.find(".button.action").click(function() {
-      return form.submit();
-    });
-    return button.click(function() {
-      if (button.hasClass("reject") || button.hasClass("delete")) {
-        return modal.modal('show');
-      }
-      return form.submit();
-    });
   });
 });
