ffdd3e39545cbbd01e816a3051fd06a55f47d4b0
speedingdeer <ffwisniewski@gmail.com>
2016-09-28 16:39:20 +0200

    Deployed assets.

--- assets/build/js/candidate.js
+++ assets/build/js/candidate.js
@@ -13,7 +13,7 @@ $(function() {
       });
     }
   });
-  $(".modal.send-email").each(function() {
+  $(".modal.sendemail").each(function() {
     var initial_message, modal;
     modal = $(this);
     initial_message = modal.find("textarea[name='message']").val();
@@ -23,5 +23,24 @@ $(function() {
       return modal.find("form.sendmail input[name='message']").val(initial_message);
     });
   });
-  return $('.container.candidate .menu .item').tab();
+  $('.container.candidate .menu .item').tab();
+  $('.modal.addsubmission .dropdown.stage').dropdown();
+  $('.modal.addsubmission .dropdown.job').dropdown();
+  $('.modal.addsubmission .dropdown.client').dropdown({
+    'onChange': function(v) {
+      $('.modal.addsubmission .dropdown.job div.item').hide();
+      $('.modal.addsubmission .dropdown.job div.item.client-id-' + v).show();
+      return $('.modal.addsubmission .dropdown.job').dropdown('clear');
+    }
+  });
+  $('.modal.addsubmission form').form({
+    fields: {
+      clientId: 'empty',
+      stageId: 'empty',
+      jobId: 'empty'
+    }
+  });
+  return $('.modal.addsubmission button[type="submit"]').click(function() {
+    return $('.modal.addsubmission form').submit();
+  });
 });
