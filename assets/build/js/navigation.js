1333dd8d211e194fee05b0787bd2bb30ba86b8a0
speedingdeer <ffwisniewski@gmail.com>
2016-10-12 15:57:53 +0200

    Deployed assets.

--- assets/build/js/navigation.js
+++ assets/build/js/navigation.js
@@ -1,26 +1,7 @@
 $(function() {
-  var navbar, popup;
+  var navbar;
   navbar = $("#navbar");
-  if ($('.ui.sidebar.candidate-search').length === 0) {
-    popup = navbar.find("a.search").popup({
-      on: 'click',
-      closable: false,
-      hoverable: false,
-      position: 'bottom center',
-      preserve: true,
-      lastResort: true,
-      closable: false,
-      duration: 200,
-      exclusive: true,
-      onVisible: function() {
-        return $("#search-form input").first().focus();
-      }
-    });
-  }
   return navbar.find(".dropdown.bars").dropdown({
-    on: 'hover',
-    onShow: function() {
-      return popup.popup('hide');
-    }
+    on: 'hover'
   });
 });
