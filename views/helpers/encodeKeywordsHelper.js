5751456d16b804b4466a5cf2b5e69ca73a7af380
speedingdeer <ffwisniewski@gmail.com>
2016-10-14 17:33:56 +0200

    Deleted old not used js. Deleted some weid js -> html handlebars helpers.

--- views/helpers/encodeKeywordsHelper.js
+++
@@ -1,3 +0,0 @@
-module.exports = function encodeKeywordsHelper(keywords) {
-  return encodeURIComponent(keywords);
-};
--- views/helpers/statusStageHelper.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (id, stages) {
-
-  var stagesObj = stages.filter(function (stage) {
-    return stage.id.toString() === id.toString();
-  });
-
-  return stagesObj[0].name;
-};
