335574172807889eb1f220c59629376ec0255526
speedingdeer <ffwisniewski@gmail.com>
2016-10-18 18:28:30 +0200

    Deployed assets.

--- assets/build/js/search.js
+++ assets/build/js/search.js
@@ -63,7 +63,7 @@ $(function() {
   enable_search_button();
   clear_button = search_form.find(".advance_search button.clear");
   search_form.find(".advance_search .button.eu").click(function() {
-    search_form.find(".advance_search input[name='location']").val("Austria, Belgium, Bulgaria, Croatia, Republic of Cyprus, Czech Republic, Denmark, Estonia, Finland, France, Germany, Greece, Hungary, Ireland, Italy, Latvia, Lithuania, Luxembourg, Malta, Netherlands, Poland, Portugal, Romania, Slovakia, Slovenia, Spain, Sweden, United Kingdom");
+    search_form.find(".advance_search input[name='location']").val("Austria, Belgium, Bulgaria, Croatia, Cyprus, Czech Republic, Denmark, Estonia, Finland, France, Germany, Greece, Hungary, Ireland, Italy, Latvia, Lithuania, Luxembourg, Malta, Netherlands, Poland, Portugal, Romania, Slovakia, Slovenia, Spain, Sweden, United Kingdom");
     enable_search_option();
     return enable_search_button();
   });
