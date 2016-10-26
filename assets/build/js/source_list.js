b625e1bebc93e8f406f796fb82c84b726c6eb0e4
speedingdeer <ffwisniewski@gmail.com>
2016-10-26 16:35:59 +0200

    Deployed assets.

--- assets/build/js/source_list.js
+++ assets/build/js/source_list.js
@@ -4,9 +4,27 @@ $(function() {
   if (container.length === 0) {
     return;
   }
-  return container.find('.tabular.menu .item').tab({
+  container.find('.tabular.menu .item').tab({
     history: true,
     historyType: 'hash',
     onVisible: function(t) {}
+  });
+  return container.find('.ui.segment.head i.edit').each(function() {
+    var button, modal, modal_form;
+    button = $(this);
+    modal = button.parent().find(".modal.edit-folder");
+    modal_form = modal.find("form");
+    modal_form.form({
+      fields: {
+        name: 'empty'
+      }
+    });
+    modal.find(".actions button.update").click(function() {
+      modal.find("form").submit();
+      return false;
+    });
+    return button.click(function() {
+      return modal.modal('show');
+    });
   });
 });
