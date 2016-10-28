0b1276b67ad46f68dbc3729df1f8c5ab4809de28
speedingdeer <ffwisniewski@gmail.com>
2016-10-28 19:27:56 +0200

    Deployed assets.

--- assets/build/js/email_modal.js
+++ assets/build/js/email_modal.js
@@ -1,34 +1,95 @@
 $(function() {
-  return $(".modal.sendemail").each(function() {
-    var modal;
-    modal = $(this);
-    modal.find(".ui.accordion").accordion({
-      'onOpen': function() {
-        return create_email_template_modal.modal('refresh');
+  var email_content, form, modal;
+  modal = $(".modal.sendemail");
+  modal.find(".ui.accordion").accordion({
+    'onOpen': function() {
+      return modal.modal('refresh');
+    }
+  });
+  form = modal.find("form.sendmail");
+  email_content = (function(_this) {
+    return function() {
+      var candidate_firstname, candidate_fullname, clientId, content, jobId;
+      content = form.find("textarea").val();
+      clientId = form.find(".dropdown.client").find("input[name='clientId']").val();
+      jobId = form.find(".dropdown.job").find("input[name='jobId']").val();
+      candidate_fullname = form.find(".template_content.candidatefullname").html();
+      candidate_firstname = form.find(".template_content.candidatefirstname").html();
+      if (clientId) {
+        content = content.replace('{{client.name}}', form.find(".template_content.clientname." + clientId).html());
       }
-    });
-    return modal.find("form.sendmail").each(function() {
-      var form;
-      form = $(this);
-      $(this).form({
-        fields: {
-          subject: 'empty',
-          message: 'empty'
-        }
-      });
-      $(this).submit(function() {
-        return modal.modal('refresh');
-      });
-      return $(this).find(".dropdown.template-selection").dropdown({
-        onChange: function(f) {
-          var html;
-          html = "";
-          if (f) {
-            html = form.find(".template_content." + f).html();
+      if (jobId) {
+        content = content.replace('{{job.title}}', form.find(".template_content.jobtitle." + jobId).html());
+        content = content.replace('{{job.address.country}}', form.find(".template_content.jobaddresscountry." + jobId).html());
+        content = content.replace('{{job.address.city}}', form.find(".template_content.jobaddresscity." + jobId).html());
+        content = content.replace('{{job.employmenttype}}', form.find(".template_content.jobemploymenttype." + jobId).html());
+      }
+      if (candidate_fullname) {
+        content = content.replace('{{candidate.fullname}}', candidate_fullname);
+      } else {
+        content = content.replace('{{candidate.fullname}}', candidate_fullname);
+      }
+      if (candidate_firstname) {
+        content = content.replace('{{candidate.firstname}}', candidate_firstname);
+      } else {
+        content = content.replace('{{candidate.firstname}}', "[FULL NAME]");
+      }
+      return content;
+    };
+  })(this);
+  $.fn.form.settings.rules.variables_defined = (function(_this) {
+    return function(value) {
+      var content;
+      content = email_content();
+      console.log(content);
+      console.log(content.match(/{{.*}}/));
+      return !content.match(/{{.*}}/);
+    };
+  })(this);
+  form.form({
+    fields: {
+      subject: {
+        identifier: 'subject',
+        rules: [
+          {
+            type: 'empty'
           }
-          return form.find("textarea").html(html);
-        }
-      });
-    });
+        ]
+      },
+      message: {
+        identifier: 'message',
+        rules: [
+          {
+            type: 'empty'
+          }, {
+            type: 'variables_defined[form]',
+            prompt: 'Some template\'s variables don\'t have defined values. Check preview to identify them'
+          }
+        ]
+      }
+    }
+  });
+  form.submit(function() {
+    return modal.modal('refresxh');
+  });
+  form.find(".dropdown.template-selection").dropdown({
+    onChange: function(f) {
+      var content, subject;
+      content = "";
+      subject = "";
+      if (f) {
+        content = form.find(".template_content.content." + f).html();
+      }
+      if (f) {
+        subject = form.find(".template_content.subject." + f).html();
+      }
+      form.find("textarea").html(content);
+      return form.find("input[name='subject']").val(subject);
+    }
+  });
+  return modal.find(".menu .item").tab({
+    onVisible: function(t) {
+      return modal.find("pre.email_content").html(email_content());
+    }
   });
 });
